315e0323716ae2b9dae0e3c373e7fec6fa92c77e
David73-Coloa <xdsomu851@gmail.com>
2022-12-10 06:45:34 -0300

    Just some changes, after a year

--- Hbridge_motors.cpp
+++ Hbridge_motors.cpp
@@ -1,56 +1,57 @@
 #include <Hbridge_motors.h>
+#define HIGH true
+#define LOW false
 Hbridge_motors::Hbridge_motors(L298H* motor1,L298H* motor2) {
-	this -> m1 = motor1;
-	this -> m2 = motor2;
+  this -> m1 = motor1;
+  this -> m2 = motor2;
+}
 
+void Hbridge_motors::turnPin(bool state1,bool state2,bool state3,bool state4) {
+  digitalWrite(m1 -> pin1,state1);
+  digitalWrite(m1 -> pin2,state2);
+  digitalWrite(m2 -> pin1,state3);
+  digitalWrite(m2 -> pin2,state4);
+}
+
+void Hbridge_motors::run(int speedToRun=1000,int _speedToRun=1000) {
+  if (speedToRun == 1000 && _speedToRun == 1000) { // The speed was not passed 
+    analogWrite(m1 -> enable,m1 -> speed);
+    analogWrite(m2 -> enable,m2 -> speed);
+  }else {
+    analogWrite(m1 -> enable,speedToRun);
+    analogWrite(m2 -> enable,_speedToRun);
+  }
 }
 
 void Hbridge_motors::goToRight() {
-	digitalWrite(m1 -> pin1,HIGH);
-	digitalWrite(m1 -> pin2,LOW);
-	digitalWrite(m2 -> pin1,LOW);
-	digitalWrite(m2 -> pin2,HIGH);
-	analogWrite(m1 -> enable,m1 -> speed);
-	analogWrite(m2 -> enable,m2 -> speed);
+  // H L L H 
+  turnPin(HIGH,LOW,LOW,LOW);
+  run();
 }
 
 void Hbridge_motors::goToLeft() {
-	digitalWrite(m1 -> pin1,LOW);
-	digitalWrite(m1 -> pin2,HIGH);
-	digitalWrite(m2 -> pin1,HIGH);
-	digitalWrite(m2 -> pin2,LOW);
-	analogWrite(m1 -> enable,m1 -> speed);
-	analogWrite(m2 -> enable,m2 -> speed);
+  // L H H L
+  turnPin(LOW,HIGH,HIGH,LOW);
+  run();	
 }
 
 void Hbridge_motors::goForward() {
-	digitalWrite(m1 -> pin1,HIGH);
-	digitalWrite(m1 -> pin2,LOW);
-	digitalWrite(m2 -> pin1,HIGH);
-	digitalWrite(m2 -> pin2,LOW);
-	analogWrite(m1 -> enable,m1 -> speed);
-	analogWrite(m2 -> enable,m2 -> speed);
+  // H L H L
+  turnPin(HIGH,LOW,HIGH,LOW);
+  run();
 }
 
 void Hbridge_motors::goBackward() {
-	digitalWrite(m1 -> pin1,LOW);
-	digitalWrite(m1 -> pin2,HIGH);
-	digitalWrite(m2 -> pin1,LOW);
-	digitalWrite(m2 -> pin2,HIGH);
-	analogWrite(m1 -> enable,m1 -> speed);
-	analogWrite(m2 -> enable,m2 -> speed);
+  //L H L 
+  turnPin(LOW,HIGH,HIGH,LOW);
+  run();
 }
 
 void Hbridge_motors::stop() {
-	digitalWrite(m1 -> pin1,LOW);
-	digitalWrite(m1 -> pin2,LOW);
-	digitalWrite(m2 -> pin1,LOW);
-	digitalWrite(m2 -> pin2,LOW);
-	analogWrite(m1 -> enable,0);
-	analogWrite(m2 -> enable,0);
+  turnPin(LOW,LOW,LOW,LOW);
+  // L L L L
+  run(0,0);
 }
 
-void Hbridge_motors::setSpeed(byte speedM1,byte speedM2) {
-	m1 -> speed = speedM1;
-	m2 -> speed = speedM2;
-}
+
+void Hbridge_motors::setSpeed(int speedM1,int speedM2) { m1 -> speed = speedM1; m2 -> speed = speedM2; }
--- Hbridge_motors.h
+++ Hbridge_motors.h
@@ -5,15 +5,17 @@
 
 class Hbridge_motors {
   public:
-  	L298H* m1 = new L298H();
-	  L298H* m2 = new L298H();
-	  Hbridge_motors(L298H* motor1, L298H* motor2);
-		void goToLeft();
-		void goToRight();
-		void setSpeed(byte speedM1,byte speedM2);
-		void stop();
-		void goForward();
-		void goBackward();
+    L298H* m1 = new L298H();
+    L298H* m2 = new L298H();
+    Hbridge_motors(L298H* motor1, L298H* motor2);
+    void goToLeft();
+    void goToRight();
+    void setSpeed(int speedM1,int speedM2);
+    void stop();
+    void goForward();
+    void goBackward();
+    void run(int speedToRun=1000,int _speedToRun=1000);
+    void turnPin(bool state1,bool state2,bool state3,bool state4);
 };
 
 #endif //Hbridge_motors_H 
